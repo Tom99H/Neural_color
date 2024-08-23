53f2200abc8a5e8353d1e6804a2c85eb7c64a3fc
LukasPetrak <125558116+LukasPetrak@users.noreply.github.com>
2024-08-23 18:45:53 +0200

    Popis provedených změn

---
+++ Neuronka/neuronka.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+int i1;
+int i2;
+int i3;
+int w;
+int kinosal[6][3];
+int main(void){
+// Naplnění nulami
+for (int j = 0; j < 3; j++){
+    for (int i = 0; i < 6; i++){
+        kinosal[i][j] = 0;
+    }
+}
+return 0;
+}
