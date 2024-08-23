41a843f5bb40c5c43cae66192f4236587ddfe6ae
LukasPetrak <125558116+LukasPetrak@users.noreply.github.com>
2024-08-23 19:17:10 +0200

    GG

--- Neuronka/neuronka.cpp
+++ Neuronka/neuronka.cpp
@@ -7,7 +7,23 @@
 int i1;
 int i2;
 int i3;
-int w;
+int w11;
+int w12;
+int w13;
+int w14;
+int w15;
+int w21;
+int w22;
+int w23;
+int w24;
+int w25;
+int w31;
+int w32;
+int w33;
+int w34;
+int w35;
+
+
 int kinosal[6][3];
 int main(void){
 // Naplnění nulami
